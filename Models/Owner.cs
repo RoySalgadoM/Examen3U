e5f6c0a20964ebd3297fcfe399c6c06cca9d86e1
Roy Salgado <20203tn052@utez.edu.mx>
2023-08-18 19:19:17 -0600

    add: Owner model

---
+++ Models/Owner.cs
@@ -0,0 +1,13 @@
+namespace Examen3U.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? SecondSurname { get; set; }
+        public string? Address  { get; set; }
+        public string? Email  { get; set; }
+        public string? Phone  { get; set; }
+    }
+}
